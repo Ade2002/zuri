d0e5e18e943ef9e54324c3409bb8f6c1f46efd4b
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-24 21:17:01 +0100

    adding integers in python

---
+++ addInteger.py
@@ -0,0 +1,9 @@
+# Store input numbers
+integer1 = input('Pls enter first number: ')
+integer2 = input('Pls enter second number: ')
+
+# Add two numbers
+sum = float(integer1) + float(integer2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(integer1, integer2, sum))
---
+++ countWord.py
@@ -0,0 +1,4 @@
+a_string = "you string here"
+word_list = a_string.split()
+number_of_words = len(word_list)
+print(number_of_words)
